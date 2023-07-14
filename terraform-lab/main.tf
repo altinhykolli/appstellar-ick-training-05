d0c8c0c4420fe42b5848d09475bc05d69454d9bd
altinhykolli <altinhykolli@gmail.comm>
2023-07-14 20:52:04 +0200

    added s3 bucket

--- terraform-lab/main.tf
+++ terraform-lab/main.tf
@@ -4,6 +4,15 @@ data "aws_vpc" "this" {
   }
 }
 
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket-altin-devops-training"
+
+  tags = {
+    Name        = "altin devops training"
+    Environment = "Dev"
+  }
+}
+
 output "vpc_id" {
   value = data.aws_vpc.this.id
 }
